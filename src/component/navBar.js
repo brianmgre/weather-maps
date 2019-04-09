d2fad24cc846109b966b566b1cb2b1f89dfa9adc
Brian G <brian.gregorius@gmail.com>
2019-04-09 00:37:58 -0400

    changed navbar a little

--- src/component/navBar.js
+++ src/component/navBar.js
@@ -1,17 +1,22 @@
 import React from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { styles } from "./styles/navBarStyle";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 const Navbar = props => {
+  const { classes } = props;
+
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography variant="h3">World Weather</Typography>
+    <AppBar className={classes.root}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h3" className={classes.logoText}>
+          World Weather
+        </Typography>
       </Toolbar>
     </AppBar>
   );
 };
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
